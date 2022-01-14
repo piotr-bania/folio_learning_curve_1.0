85ce0a1945f1e0e313ecdaa1458f4546e6c4e8d0
Piotr Bania <piotr.bania@europe.com>
2022-01-14 15:48:49 +0000

    init

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,16 @@
 import * as THREE from '../node_modules/three/build/three.module.js'
+import {
+    gsap
+} from '../node_modules/gsap/src/gsap-core.js'
+import {
+    ExpoScaleEase,
+    RoughEase,
+    SlowMo
+} from '../node_modules/gsap/src/EasePack.js'
+
+gsap.registerPlugin(TextPlugin, ExpoScaleEase, RoughEase, SlowMo);
 
 console.log(THREE)
 
 // Canvas
 const canvas = document.querySelector('canvas.canvas')
-
